593aedfcbc05f73ab6a784791f62976a2a69e271
HawkSWE <hook200364@gmail.com>
2022-02-09 10:29:42 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,8 @@
-function card(){
+fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=ka73KGDAJdvE4e97W71B4GMTkwBPwoAa")
+    .then(response => {
+        
+    })
+    .catch(error => {
+        
+    });
 
-}
-https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=ka73KGDAJdvE4e97W71B4GMTkwBPwoAa
